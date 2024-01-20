06981a6de541d46ab229ccd73a574082fcecb285
CrypTe <102381890+GamingSeries@users.noreply.github.com>
2024-01-20 00:55:23 -0500

    nothing additional

--- components/mycomponents.tsx
+++ components/mycomponents.tsx
@@ -2,17 +2,25 @@ import React from 'react';
 
 const MyComponent: React.FC = () => {
   return (
-    <div className={`bg-rgba(var(--foreground-rgb), 0.7)`}>
+    <div className="w-[1512px] h-[1843px] relative bg-gradient-to-b from-rose-700 to-black shadow">
+      {/* Content */}
       <div className="flex justify-center items-start h-screen">
-        <div className="w-[1512px] text-center text-white text-[40px] font-normal font-Karma">
-            Muneeb's Portfolio
+        <div className="w-[1512px] text-center text-white text-5xl font-normal font-Karma">
+          My Portfolio
         </div>
       </div>
 
       {/* Other sections with different background colors */}
-      <div className="bg-rgba(var(--foreground-rgb), 0.5)">
+      <div className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500"></div>
         {/* Content with a transparent background */}
       </div>
+
+      {/* Profile picture */}
+      <img
+        className="w-[70px] h-[70px] left-[20px] top-[16px] absolute rounded-full"
+        src="/images/23bc50b4-9ba0-423a-a149-279afc732e98.JPG"
+        alt="Profile"
+      />
     </div>
   );
 };
