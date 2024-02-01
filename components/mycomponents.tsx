79469cce54fc2341cc61bb34b5da06947ff02111
CrypTe <102381890+GamingSeries@users.noreply.github.com>
2024-02-01 11:25:05 -0500

    added proper indention for the image

--- components/mycomponents.tsx
+++ components/mycomponents.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Image from 'next/image';
 
 const MyComponent: React.FC = () => {
   return (
@@ -11,10 +12,12 @@ const MyComponent: React.FC = () => {
       {/* Profile picture and About Me Section */}
       <div className="flex items-start">
         {/* Profile picture */}
-        <img
+        <Image
           className="w-[40px] h-[40px] md:w-[50px] md:h-[50px] lg:w-[60px] lg:h-[60px] absolute rounded-full"
           src="/images/23bc50b4-9ba0-423a-a149-279afc732e98.JPG"
           alt="Profile"
+          width={60}  // Set the width you need
+          height={60} // Set the height you need
           style={{ left: '20px', top: '16px' }}
         />
       </div>
@@ -23,13 +26,14 @@ const MyComponent: React.FC = () => {
         About Me:
       </div>
 
-        <div className='text-white'>
-          Hey there! 🚀 I'm a 3rd-year Computer Science student on a mission to become a software developer, and let me tell you, it's been one heck of an adventure! 🌟
-          <br />
-          Every day, I'm diving into the fascinating world of coding, and it feels like I'm crafting my own digital masterpiece. It's not just about learning languages and algorithms; it's about unleashing creativity and problem-solving skills. I'm not just coding; I'm painting with pixels and building the future, one line at a time.
-          Debugging is like being a tech detective solving virtual mysteries, and overcoming challenges feels like reaching the summit of a digital mountain. With every new concept, it's like unlocking a secret level in my own coding game, bringing me one step closer to being a coding maestro.
-        </div>
+      <div className='text-white'>
+        Hey there! 🚀 I'm a 3rd-year Computer Science student on a mission to become a software developer, and let me tell you, it's been one heck of an adventure! 🌟
+        <br />
+        Debugging is like being a tech detective solving virtual mysteries, and overcoming challenges feels like reaching the summit of a digital mountain. 
+        <br />
+        With every new concept, it's like unlocking a secret level in my own coding game, bringing me one step closer to being a coding maestro.
       </div>
+    </div>
   );
 };
 
